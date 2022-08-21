267c7a0987637c7aa24ae8ff2add074d537f6f30
Dmitriy Kurzykin <ametek84@mail.ru>
2022-08-21 19:06:32 +0300

    Task 2 complete

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 def open_recipes_file():
     cook_book = {}
     with open('./files/recipes.txt', 'r', encoding='utf-8') as file:
@@ -14,7 +16,22 @@ def open_recipes_file():
             null_string = file.readline()
             if null_string == "":
                 break
-    return (cook_book)
 
-from pprint import pprint
-pprint(open_recipes_file(), sort_dicts=False)
+    return cook_book
+
+
+def get_shop_list_by_dishes(dishes, person_count):
+    recipe_dish = {}
+    cook_book = open_recipes_file()
+    for dish, ingredients in cook_book.items():
+        if dish in dishes:
+            for ingredient in ingredients:
+                total_quantity = int(ingredient['quantity']) * person_count
+                recipe_dish[ingredient['ingredient_name']] = {'measure': ingredient['measure'],
+                                                              'quantity': total_quantity}
+    pprint(recipe_dish, sort_dicts=False)
+    return recipe_dish
+
+
+# pprint(open_recipes_file(), sort_dicts=False)
+get_shop_list_by_dishes(['Фахитос', 'Омлет'], 3)
